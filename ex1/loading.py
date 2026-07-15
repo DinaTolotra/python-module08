f680b25d85c51ac0a9f8bda41d88298e91799465
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-15 21:47:26 +0300

    giga improvment

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,11 +1,14 @@
 from importlib.util import find_spec
 from importlib.metadata import version
+from importlib import import_module
 from sys import prefix, base_prefix
+from sys import stderr
 
 
 def check_env() -> None:
     pkg_list: list[str] = [
         "numpy", "pandas", "matplotlib"]
+    pkg_missing: list[str] = []
     if prefix == base_prefix:
         raise RuntimeError(
             "virtual environment not found")
@@ -15,58 +18,58 @@ def check_env() -> None:
                   f"({version(pkg)})")
         else:
             print(f"[KO] {pkg}")
-            raise RuntimeError(
-                f"package '{pkg}' not found")
+            pkg_missing.append(pkg)
+    if pkg_missing:
+        raise RuntimeError(
+            f"missing packages: {', '.join(pkg_missing)}")
 
 
 def print_usage() -> None:
     print("Usage:")
     print("\n$ # pip")
     print("$ python -m venv venv")
+    print(" or \n$ /opt/pyenv/versions/3.13.*/bin/python -m venv venv")
     print("$ source venv/bin/activate{.sh|.fish}")
     print("$ pip install -r requirements.txt")
     print("$ python loading.py")
+    print("$ # Tool used: pip, venv, ./requirements.txt")
     print("\n$ # poetry")
     print("$ poetry install")
-    print("$ poetry env use python3.13")
-    print("$ poetry run python3.13 loading.py")
+    print("$ poetry env use python3.13", end="")
+    print(" # or /opt/pyenv/versions/3.13.*/bin/python")
+    print("$ poetry run python loading.py")
+    print("$ # Tool used: poetry, ./pyproject.toml")
+
+
+def main() -> None:
+    pd = np = plt = None
+    try:
+        check_env()
+        pd = import_module("pandas")
+        np = import_module("numpy")
+        plt = import_module("matplotlib.pyplot")
+    except RuntimeError as e:
+        print(f"[Error] {e}\n", file=stderr)
+        print_usage()
+        return
+
+    rng = np.random.default_rng()
+    x = np.arange(100)
+    y = x * rng.random(100)
+
+    df1 = pd.DataFrame({
+        "x": x,
+        "y": y
+    })
+    df2 = pd.DataFrame({
+        "x": x,
+        "y": np.cumsum(y) / np.arange(1, len(y) + 1)
+    })
+
+    plt.plot(df1["x"], df1["y"])
+    plt.plot(df2["x"], df2["y"])
+    plt.savefig("output.png")
 
 
 if __name__ == "__main__":
-    try:
-        print("[LOG] Checking deps...")
-        check_env()
-        import numpy as np
-        import pandas as pd
-        import matplotlib.pyplot as plt
-    except RuntimeError as e:
-        print("Error:", e)
-        print_usage()
-    else:
-        count = 100
-        rng = np.random.default_rng()
-        num = np.arange(count)
-        dates = np.arange(
-            '2026-06-01',
-            '2026-06-10',
-            dtype=np.datetime64)
-        print("\n[LOG] Generating data...")
-        score = rng.uniform(0, 100, (len(dates), count))
-        score = np.round(score, 2)
-        print("[LOG] Arranging data...")
-        raw_df = pd.DataFrame(
-            score,
-            columns=num,
-            index=dates)
-        avg_df = raw_df.mean()
-        print("=== DataFrame ===")
-        print(raw_df.info(verbose=False))
-        print("=================")
-        print("[LOG] Generating visualization...")
-        avg_df.plot(
-            title="player score average",
-            xlabel="num",
-            ylabel="avg"
-        )
-        plt.savefig("player_score.png")
-        print("[LOG] Figure saved to: player_score.png")
+    main()
