1c0b0dd190c2f1d2ea2ef201ef0319e05a3da43a
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-15 20:32:04 +0300

    prettieing

--- ex0/construct.py
+++ ex0/construct.py
@@ -3,52 +3,51 @@ import os
 import site
 
 
-def is_venv() -> bool:
+def is_virtual_env() -> bool:
     return sys.prefix != sys.base_prefix
 
 
 def print_venv_info() -> None:
-    print("Virtual Environment:", end=" ")
-    if is_venv():
-        venv_path: str | None = os.getenv("VIRTUAL_ENV")
-        if venv_path:
+    print("Virtual Environment: ", end="")
+    venv_path: str | None
+    if is_virtual_env():
+        venv_path = os.getenv("VIRTUAL_ENV")
+        if venv_path is not None:
             print(os.path.basename(venv_path))
-        print(
-            "Environment Path:",
-            venv_path
-        )
+        print("Environment Path:", venv_path)
     else:
         print("None detected")
 
 
 def print_site_info() -> None:
-    if is_venv():
+    if is_virtual_env():
         print("Package installation path:")
         print(site.getsitepackages()[0])
 
 
-if __name__ == "__main__":
+def main() -> None:
     print("MATRIX STATUS:", end=" ")
-    if is_venv():
+    if is_virtual_env():
         print("Welcome to the construct")
     print("Current Python:", sys.executable)
     print_venv_info()
     print("")
 
-    if is_venv():
-        print(
-            "SUCCESS: You're in an isolated environment!",
-            "Safe to install packages without affecting the global system.\n",
-            sep="\n"
-        )
+    if is_virtual_env():
+        print("SUCCESS: You're in an isolated environment!\n",
+              "Safe to install packages without",
+              "affecting the global system.\n")
     else:
-        print(
-            "WARNING: You're in the global environment!",
-            "The machines can see everything you install.\n",
-            "python -m venv matrix_env",
-            "source matrix_env/bin/activate # On Unix",
-            "matrix_env\\Scripts\\activate # On Windows\n",
-            "Then run this program again.",
-            sep="\n"
-        )
+        print("WARNING: You're in the global environment!\n",
+              "The machines can see everything you install.\n\n",
+              "To create an isolated environment, ",
+              "run the following commands:\n",
+              "$ python -m venv matrix_env\n",
+              "$ source matrix_env/bin/activate # On Unix\n",
+              "$ matrix_env\\Scripts\\activate # On Windows\n\n",
+              "Then run this program again.")
     print_site_info()
+
+
+if __name__ == "__main__":
+    main()
